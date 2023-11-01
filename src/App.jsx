52cff8288e6a4ed043a5590c51932b8b20357347
renewensel <hey@renewensel.com>
2023-11-01 09:40:28 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <div>
+                <form>
+                    <h1>Weather Mood App</h1>
+                    <div>
+                        <h4>Add an activity:</h4>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                        <label htmlFor="name">Name: </label>
+                        <input
+                            required
+                            type="text"
+                            placeholder="Add a new activity here..."
+                            id="name"
+                            name="name"
+                            maxlength="30"
+                            minlength="2"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="checkbox">Good weather activity?</label>
+                        <input type="checkbox" />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
